128d2648b4d4ce5f6a00e7a4d21d0b3eb3ec4eab
Miomir Paripovic <deploy.friday@gmail.com>
2023-11-14 18:43:59 +0100

    JSX, final part

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       animal: "Dog",
-//       name: "Luna",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       animal: "Bird",
-//       name: "Pepper",
-//       breed: "cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       animal: "Cat",
-//       name: "Doink",
-//       breed: "Mixed",
-//     }),
-//   ]);
-// };
-
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="Cat" breed="Mixed" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(<App />));
+root.render(<App />);
