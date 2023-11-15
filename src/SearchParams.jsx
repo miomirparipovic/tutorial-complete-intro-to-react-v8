7c94581d44570661490d23653dd3ded0f75d338e
Miomir Paripovic <deploy.friday@gmail.com>
2023-11-15 16:38:38 +0100

    Hooks, final (v.3)

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -4,6 +4,8 @@ const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -20,13 +22,30 @@ const SearchParams = () => {
         <label htmlFor="animal">
           Animal
           <select
-            onChange={(e) => setAnimal(e.target.value)}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
             id="animal"
             value={animal}
           >
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => setBreed(e.target.value)}
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
