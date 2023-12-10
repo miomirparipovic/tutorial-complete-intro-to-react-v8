8aa6401375b2a6825233775e92e099f0f7341c7a
Miomir Paripovic <deploy.friday@gmail.com>
2023-12-10 15:39:20 +0100

    Error boundaries, (v.6)

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Details = () => {
   const { id } = useParams();
@@ -32,4 +33,10 @@ const Details = () => {
   );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
